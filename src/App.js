6ea23a3ed4a4cb5af991dd290b310c04c1d8d6c8
rotome <rotome@unirioja.es>
2019-11-18 13:53:29 +0100

    React app using functions

--- src/App.js
+++ src/App.js
@@ -1,19 +1,21 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import Person from './Person/Person';
 
-class App extends Component {
-  state = {
+const App = props => {
+  const [personsState, setPersonsState] = useState({
     persons: [
       {name: "Rodrigo", age: 21},
       {name: "Regina",  age: 23}
-    ],
-    otherProperty: 'Other property',
-  }
+    ]
+  });
 
-  switchNameHandler = () => {
-    // console.log("I was click!!");
-    this.setState({
+  const [otherState, setOtherState] = useState({otherProperty: 'Other property'});
+
+  console.log(personsState, otherState);
+
+  const switchNameHandler = () => {
+    setPersonsState({
       persons: [
         {name: "rotome", age: 21},
         {name: "Regina", age: 23}
@@ -21,19 +23,17 @@ class App extends Component {
     });
   }
 
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <h1>Hello world</h1>
-          <button onClick={this.switchNameHandler}>Switch Name</button>
-          <Person name={this.state.persons[0].name} age={this.state.persons[0].age}>My Hobbies: Painting</Person>
-          <Person name={this.state.persons[1].name} age={this.state.persons[1].age} />
-          <p>{this.state.otherProperty}</p>
-        </header>
-      </div>
-    );
-  };
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Hello world</h1>
+        <button onClick={switchNameHandler}>Switch Name</button>
+        <Person name={personsState.persons[0].name} age={personsState.persons[0].age}>My Hobbies: Painting</Person>
+        <Person name={personsState.persons[1].name} age={personsState.persons[1].age} />
+        <p>{otherState.otherProperty}</p>
+      </header>
+    </div>
+  );
 }
 
 export default App;
