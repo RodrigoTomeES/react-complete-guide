266e65c49348b50c8e679b2d0ac7fa97b1643bdf
RodrigoTomeES <tomecafeconrodrigo@live.com>
2019-11-26 16:00:53 +0000

    Updates

---
+++ precache-manifest.4da304ca1eadbe56ff0c99c15f051e2e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "40ea4902b75cb37492d92dce6209421c",
+    "url": "/react-complete-guide/index.html"
+  },
+  {
+    "revision": "0d971b91027d5f1f98e1",
+    "url": "/react-complete-guide/static/css/main.ee5e3148.chunk.css"
+  },
+  {
+    "revision": "ab4996c01915e45e4019",
+    "url": "/react-complete-guide/static/js/2.b6b1b066.chunk.js"
+  },
+  {
+    "revision": "0d971b91027d5f1f98e1",
+    "url": "/react-complete-guide/static/js/main.5e9debcc.chunk.js"
+  },
+  {
+    "revision": "5458183b97839e7fefb8",
+    "url": "/react-complete-guide/static/js/runtime-main.0d560d67.js"
+  }
+]);
