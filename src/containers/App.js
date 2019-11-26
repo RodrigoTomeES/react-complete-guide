7686dc33e194fc6a4d135456a92e29f89bec9bab
RodrigoTomeES <tomecafeconrodrigo@live.com>
2019-11-26 13:53:55 +0100

    Rendering adjacent JSX element with Higher Order Components (HOC)

--- src/containers/App.js
+++ src/containers/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import classes from './App.module.css';
 import Persons from '../components/Persons/Persons';
 import Cockpit from '../components/Cockpit/Cockpit';
+import WithClass from '../hoc/WithClass';
 
 class App extends Component {
   constructor(props) {
@@ -79,7 +80,7 @@ class App extends Component {
     }
 
     return (
-      <div className={classes.App}>
+      <WithClass classes={classes.App}>
         <header className={classes.AppHeader}>
           <button
             onClick={() => {
@@ -98,7 +99,7 @@ class App extends Component {
           ) : null}
           {persons}
         </header>
-      </div>
+      </WithClass>
     );
   };
 }
---
+++ src/hoc/Auxiliary.js
@@ -0,0 +1,5 @@
+// This is the same like React.Fragment
+
+const aux = props => props.children;
+
+export default aux;
