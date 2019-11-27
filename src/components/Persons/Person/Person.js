dd34afe6620cba93e9b8b9e1338a9938e4a2c7db
RodrigoTomeES <tomecafeconrodrigo@live.com>
2019-11-27 10:08:47 +0100

    Refactored withClass HOC to a function

--- src/components/Persons/Person/Person.js
+++ src/components/Persons/Person/Person.js
@@ -1,6 +1,7 @@
 import React, {Component} from 'react';
 import classes from './Person.module.css';
-import WithClass from '../../../hoc/WithClass';
+import withClass from '../../../hoc/withClass';
+import Aux from '../../../hoc/Auxiliary';
 
 class Person extends Component {
     render() {
@@ -12,13 +13,13 @@ class Person extends Component {
         // }
     
         return (
-            <WithClass classes={classes.Person}>
+            <Aux>
                 <p onClick={this.props.click}>I'm a {this.props.name} and I am {this.props.age} years old!</p>
                 <p>{this.props.children}</p>
                 <input type="text" onChange={this.props.changed} value={this.props.name}/>
-            </WithClass>
+            </Aux>
         );
     }
 }
 
-export default Person;
+export default withClass(Person, classes.Person);
